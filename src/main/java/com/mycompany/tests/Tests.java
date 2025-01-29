058a03181e4e993ff9e4fba6cfc2addd18d728b4
chan-shinan <shinanmohamed363@gmail.com>
2025-01-29 07:54:36 +0530

    Save progress before switching branches

--- src/main/java/com/mycompany/tests/Tests.java
+++ src/main/java/com/mycompany/tests/Tests.java
@@ -12,7 +12,8 @@ public class Tests {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-
+         System.out.println("Hello World!");
+          System.out.println("Hello World!");
         
     }
 }
